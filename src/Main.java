d82dee43fc60a02483408df17bcc5e026b01ea64
lucas-amaral <lamaral@inf.ufsm.br>
2015-03-21 15:10:20 -0300

    ajustes

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,50 @@
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     private static Main m;
     private static FileOutputStream arquivoSaida;
     private File arquivoEntrada;
 
+    public File getArquivoEntrada() {
+        return arquivoEntrada;
+    }
 
+    public void setArquivoEntrada(File arquivoEntrada) {
+        this.arquivoEntrada = arquivoEntrada;
+    }
+
+    public void getBuscaArquivo(String caminho) {
+        setArquivoEntrada(new File(caminho));
+    }
+
+    public Boolean getGravaToken(String chave) {
+        if (PalavrasReservadas.palavrasReservadas.contains(chave)) {
+            System.out.println(chave + " - palavra reservada");
+        } else if (Delimitadores.delimitadores.contains(chave)) {
+            System.out.println(chave + " - delimitador");
+        } else if (chave.startsWith("[a-A][z-Z]")) {
+            System.out.println(chave + " - identificador");
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public void inicia() {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(getArquivoEntrada()).useDelimiter("\\n");
+        } catch (FileNotFoundException e) {
+            System.out.println("[ERRO] Arquivo de entrada não encontrado " + e);
+        }
+        if (scanner != null) {
+            for (int linha = 1; scanner.hasNext() ; linha++) {
+                System.out.println(getGravaToken(scanner.toString()));
+            }
+
+        }
+    }
 
     public static void main(String []arg) throws IOException {
         try {
@@ -16,9 +52,10 @@ public class Main {
             System.setOut(new PrintStream(arquivoSaida));
         } catch (Exception e) {/**/}
         m = new Main();
-//        m.inicia();
-//        if (m.getFout() != null) {
-//            m.getFout().close();
-//        }
+        m.getBuscaArquivo(arg[0]);
+        m.inicia();
+        if (m.arquivoSaida != null) {
+            m.arquivoSaida.close();
+        }
     }
 }
